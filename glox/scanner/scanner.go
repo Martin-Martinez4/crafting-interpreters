a521809e25d626d291a130389eab6adf5d9ffb47
Martin-Martinez4 <martmartez4@gmail.com>
2025-04-25 13:51:05 -0700

    Started on the ast (expr folder, will change later)

--- glox/scanner/scanner.go
+++ glox/scanner/scanner.go
@@ -1,25 +1,52 @@
 package scanner
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/Martin-Martinez4/crafting-interpreters/glox/errorhandling"
 	"github.com/Martin-Martinez4/crafting-interpreters/glox/token"
 )
 
+var keywords = map[string]token.TokenType{
+	"var": token.VAR,
+
+	"and":   token.AND,
+	"or":    token.OR,
+	"if":    token.IF,
+	"else":  token.ELSE,
+	"true":  token.TRUE,
+	"false": token.FALSE,
+	"while": token.WHILE,
+
+	"fun":    token.FUN,
+	"return": token.RETURN,
+
+	"class": token.CLASS,
+	"this":  token.THIS,
+	"super": token.SUPER,
+
+	"print": token.PRINT,
+	"nil":   token.NIL,
+}
+
 type Scanner struct {
-	source  string
-	tokens  []token.Token
-	start   int
-	current int
-	line    int
+	source   string
+	tokens   []token.Token
+	start    int
+	current  int
+	line     int
+	keywords *(map[string]token.TokenType)
 }
 
 func NewScanner(source string) *Scanner {
 	return &Scanner{
-		source:  source,
-		tokens:  make([]token.Token, 0),
-		start:   0,
-		current: 0,
-		line:    1,
+		source:   source,
+		tokens:   make([]token.Token, 0),
+		start:    0,
+		current:  0,
+		line:     1,
+		keywords: &keywords,
 	}
 }
 
@@ -37,43 +64,105 @@ func (s *Scanner) scanTokens() {
 	s.tokens = append(s.tokens, *token.NewToken(token.EOF, "", nil, s.line))
 }
 
+func (s *Scanner) skipWhiteSpace() byte {
+	cc := s.advance()
+	for cc == ' ' || cc == '\t' || cc == '\r' {
+		s.start = s.current
+		cc = s.advance()
+	}
+
+	return cc
+}
+
 func (s *Scanner) scanToken() {
-	c := s.advance()
+	// c := s.advance()
+	c := s.skipWhiteSpace()
 
 	switch c {
 	case '(':
 		s.addToken(token.LEFT_PAREN, nil)
-		break
+
 	case ')':
 		s.addToken(token.RIGHT_PAREN, nil)
-		break
+
 	case '{':
 		s.addToken(token.LEFT_BRACE, nil)
-		break
+
 	case '}':
 		s.addToken(token.RIGHT_BRACE, nil)
-		break
+
 	case ',':
 		s.addToken(token.COMMA, nil)
-		break
+
 	case '.':
 		s.addToken(token.DOT, nil)
-	case '=':
-		s.addToken(token.EQUAL, nil)
-		break
+
 	case '+':
 		s.addToken(token.PLUS, nil)
-		break
+
 	case ';':
 		s.addToken(token.SEMICOLON, nil)
-		break
+
 	case '*':
 		s.addToken(token.STAR, nil)
-		break
+
+	case '!':
+		if s.match('=') {
+
+			s.addToken(token.BANG_EQUAL, nil)
+		} else {
+			s.addToken(token.BANG, nil)
+		}
+	case '=':
+		if s.match('=') {
+
+			s.addToken(token.EQUAL_EQUAL, nil)
+		} else {
+			s.addToken(token.EQUAL, nil)
+		}
+
+	case '<':
+		if s.match('=') {
+
+			s.addToken(token.LESS_EQUAL, nil)
+		} else {
+			s.addToken(token.LESS, nil)
+		}
+
+	case '>':
+		if s.match('=') {
+
+			s.addToken(token.GREATER_EQUAL, nil)
+		} else {
+			s.addToken(token.GREATER, nil)
+		}
+
+	case '/':
+		if s.match('/') {
+			for s.peek() != '\n' && !s.isAtEnd() {
+				s.advance()
+			}
+		} else {
+			s.addToken(token.SLASH, nil)
+		}
+
+	case '\n':
+		s.line++
+
+	case '"':
+		s.handleString()
+
 	default:
-		errorhandling.ReportAndExit(s.line, "", "unexpected character.")
-		break
+		if isDigit(c) {
+			s.handleNumber()
+		} else if IsAlpha(c) {
+			s.identifier()
+		} else {
+
+			errorhandling.ReportAndExit(s.line, "", "unexpected character.")
+		}
 	}
+
 }
 
 func (s *Scanner) advance() byte {
@@ -86,4 +175,102 @@ func (s *Scanner) addToken(t token.TokenType, literal any) {
 	text := s.source[s.start:s.current]
 	s.tokens = append(s.tokens, *token.NewToken(t, text, literal, s.line))
 
+}
+
+func (s *Scanner) match(expected byte) bool {
+	if s.isAtEnd() {
+		return false
+	}
+	if s.source[s.current] != expected {
+		return false
+	}
+
+	s.current++
+	return true
+}
+
+func (s *Scanner) charAt(index int) byte {
+	return s.source[index]
+}
+
+func (s *Scanner) peek() byte {
+	if s.isAtEnd() {
+		return 0
+	} else {
+		return s.charAt(s.current)
+	}
+}
+
+func (s *Scanner) peekNext() byte {
+	if s.current+1 >= len(s.source) {
+		return 0
+	} else {
+		return s.charAt(s.current + 1)
+	}
+}
+
+func (s *Scanner) handleString() {
+	for s.peek() != '"' && !s.isAtEnd() {
+		if s.peek() == '\n' {
+			s.line++
+		}
+		s.advance()
+	}
+
+	if s.isAtEnd() {
+		errorhandling.ReportAndExit(s.line, "", "unterminated string")
+		return
+	}
+	s.advance()
+
+	value := s.source[(s.start + 1):(s.current - 1)]
+	fmt.Println(value)
+	s.addToken(token.STRING, value)
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+func IsAlpha(b byte) bool {
+	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b == '_')
+}
+
+func isAlphaNumeric(b byte) bool {
+	return isDigit(b) || IsAlpha(b)
+}
+
+func (s *Scanner) identifier() {
+	for isAlphaNumeric(s.peek()) {
+		s.advance()
+	}
+
+	text := s.source[s.start:s.current]
+	kw, ok := (*s.keywords)[text]
+	if !ok {
+		s.addToken(token.IDENTIFIER, nil)
+	} else {
+		s.addToken(kw, nil)
+	}
+
+	s.addToken(token.IDENTIFIER, nil)
+}
+
+func (s *Scanner) handleNumber() {
+	for isDigit(s.peek()) {
+		s.advance()
+	}
+
+	if s.peek() == '.' && isDigit(s.peekNext()) {
+		s.advance()
+		for isDigit(s.peek()) {
+			s.advance()
+		}
+	}
+
+	f, err := strconv.ParseFloat(s.source[s.start:s.current], 64)
+	if err != nil {
+		errorhandling.ReportAndExit(s.line, "", "tried to treat a non-number as a number")
+	}
+	s.addToken(token.NUMBER, f)
 }
--- glox/scanner/scanner_test.go
+++ glox/scanner/scanner_test.go
@@ -44,3 +44,219 @@ func TestSimpleTokenInput(t *testing.T) {
 		}
 	}
 }
+func TestSimpleTokenInputSkipWhiteSpace(t *testing.T) {
+	input := `= +   ()
+	{}	,	;`
+
+	tests := []struct {
+		name           string
+		expectedType   token.TokenType
+		expectedLexeme string
+	}{
+		{"assign/equal sign", token.EQUAL, "="},
+		{"plus", token.PLUS, "+"},
+		{"Left Paren", token.LEFT_PAREN, "("},
+		{"Right Paren", token.RIGHT_PAREN, ")"},
+		{"Left Brace", token.LEFT_BRACE, "{"},
+		{"Right Brace", token.RIGHT_BRACE, "}"},
+		{"Comma", token.COMMA, ","},
+		{"Semicolon", token.SEMICOLON, ";"},
+		{"End of File", token.EOF, ""},
+	}
+
+	s := NewScanner(input)
+	s.scanTokens()
+
+	for i, tt := range tests {
+		tok := s.tokens[i]
+
+		fmt.Println(i, tok.Lexeme)
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedType, tok.Type)
+		}
+
+		if tok.Lexeme != tt.expectedLexeme {
+
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedLexeme, tok.Lexeme)
+		}
+	}
+}
+func TestDoubleCharInputs(t *testing.T) {
+	input := `<===!=(){},	;`
+
+	tests := []struct {
+		name           string
+		expectedType   token.TokenType
+		expectedLexeme string
+	}{
+		{"assign/equal sign", token.LESS_EQUAL, "<="},
+		{"plus", token.EQUAL_EQUAL, "=="},
+		{"plus", token.BANG_EQUAL, "!="},
+		{"Left Paren", token.LEFT_PAREN, "("},
+		{"Right Paren", token.RIGHT_PAREN, ")"},
+		{"Left Brace", token.LEFT_BRACE, "{"},
+		{"Right Brace", token.RIGHT_BRACE, "}"},
+		{"Comma", token.COMMA, ","},
+		{"Semicolon", token.SEMICOLON, ";"},
+		{"End of File", token.EOF, ""},
+	}
+
+	s := NewScanner(input)
+	s.scanTokens()
+
+	for i, tt := range tests {
+		tok := s.tokens[i]
+
+		fmt.Println(i, tok.Lexeme)
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedType, tok.Type)
+		}
+
+		if tok.Lexeme != tt.expectedLexeme {
+
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedLexeme, tok.Lexeme)
+		}
+	}
+}
+
+func TestDoubleCharInputsSkipWhiteSpace(t *testing.T) {
+	input := `<= ==
+	!=		(){},	;`
+
+	tests := []struct {
+		name           string
+		expectedType   token.TokenType
+		expectedLexeme string
+	}{
+		{"assign/equal sign", token.LESS_EQUAL, "<="},
+		{"plus", token.EQUAL_EQUAL, "=="},
+		{"plus", token.BANG_EQUAL, "!="},
+		{"Left Paren", token.LEFT_PAREN, "("},
+		{"Right Paren", token.RIGHT_PAREN, ")"},
+		{"Left Brace", token.LEFT_BRACE, "{"},
+		{"Right Brace", token.RIGHT_BRACE, "}"},
+		{"Comma", token.COMMA, ","},
+		{"Semicolon", token.SEMICOLON, ";"},
+		{"End of File", token.EOF, ""},
+	}
+
+	s := NewScanner(input)
+	s.scanTokens()
+
+	for i, tt := range tests {
+		tok := s.tokens[i]
+
+		fmt.Println(i, tok.Lexeme)
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedType, tok.Type)
+		}
+
+		if tok.Lexeme != tt.expectedLexeme {
+
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedLexeme, tok.Lexeme)
+		}
+	}
+}
+
+func TestSkipOneLineComment(t *testing.T) {
+	input := `// test
+	=  // test
+	+ / ()`
+
+	tests := []struct {
+		name           string
+		expectedType   token.TokenType
+		expectedLexeme string
+	}{
+		{"assign/equal sign", token.EQUAL, "="},
+		{"plus sign", token.PLUS, "+"},
+		{"slash", token.SLASH, "/"},
+		{"Left Paren", token.LEFT_PAREN, "("},
+		{"Right Paren", token.RIGHT_PAREN, ")"},
+	}
+
+	s := NewScanner(input)
+	s.scanTokens()
+
+	for i, tt := range tests {
+		tok := s.tokens[i]
+
+		fmt.Println(i, tok.Lexeme)
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedType, tok.Type)
+		}
+
+		if tok.Lexeme != tt.expectedLexeme {
+
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedLexeme, tok.Lexeme)
+		}
+	}
+}
+
+func TestStringLiterals(t *testing.T) {
+	input := `"Hello, world!"`
+
+	tests := []struct {
+		name            string
+		expectedType    token.TokenType
+		expectedLexeme  string
+		expectedLiteral any
+	}{
+		{"string: Hello, world!", token.STRING, `"Hello, world!"`, "Hello, world!"},
+	}
+
+	s := NewScanner(input)
+	s.scanTokens()
+
+	for i, tt := range tests {
+		tok := s.tokens[i]
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedType, tok.Type)
+		}
+
+		if tok.Lexeme != tt.expectedLexeme {
+
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedLexeme, tok.Lexeme)
+		}
+		if tok.Literal != tt.expectedLiteral {
+
+			t.Fatalf("tests[%d, %s] - Literal wrong, expected: %q got : %q", i, tt.name, tt.expectedLiteral, tok.Literal)
+		}
+	}
+
+	input = `"+=(){}"`
+
+	tests = []struct {
+		name            string
+		expectedType    token.TokenType
+		expectedLexeme  string
+		expectedLiteral any
+	}{
+		{"string: +=(){}", token.STRING, "\"+=(){}\"", "+=(){}"},
+	}
+
+	s = NewScanner(input)
+	s.scanTokens()
+
+	for i, tt := range tests {
+		tok := s.tokens[i]
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedType, tok.Type)
+		}
+
+		if tok.Lexeme != tt.expectedLexeme {
+
+			t.Fatalf("tests[%d, %s] - Lexeme wrong, expected: %q got : %q", i, tt.name, tt.expectedLexeme, tok.Lexeme)
+		}
+		if tok.Literal != tt.expectedLiteral {
+
+			t.Fatalf("tests[%d, %s] - Literal wrong, expected: %q got : %q", i, tt.name, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
