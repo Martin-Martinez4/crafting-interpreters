ea1b7c969ff056c27b4b045970fec2a0cfcfd151
martin <martmartez4@gmail.com>
2025-04-24 20:13:19 -0700

    Added some functions to Scanner, not finished yet

--- glox/scanner/scanner.go
+++ glox/scanner/scanner.go
@@ -25,3 +25,45 @@ func NewScanner(source string) *Scanner {
 func (s *Scanner) isAtEnd() bool {
 	return s.current >= len(s.source)
 }
+
+func (s *Scanner) scanToken(){
+	byte c := s.advance()
+	switch c {
+	case '(':
+		s.addToken(token.LEFT_PAREN)
+		break
+	case ')'
+		s.addToken(token.RIGHT_PAREN)
+		break
+	case '{':
+		s.addToken(token.LEFT_BRACE)
+		break
+	case '}':
+		s.addToken(token.RIGHT_BRACE)
+		break
+	case ',':
+		s.addToken(token.COMMA)
+		break
+	case '.':
+		s.addToken(token.DOT)
+		break
+	case '+':
+		s.addToken(token.PLUS)
+		break
+	case ';':
+		s.addToken(token.SEMICOLON)
+		break
+	case '*':
+		s.addToken(token.STAR)
+		break
+	}
+}
+
+func (s *Scanner) advance() byte {
+	return s.source[s.current++]
+}
+
+func (s *Scanner) addToken(type string, literal any){
+	text := s.source[s.start, s.current]
+	tokens := append(tokens, token.NewToken(type, text, literal, s.line))
+}
