b209e20cafc55429e5f988eed1590508d5964f62
Martin-Martinez-4 <martmartez4@gmail.com>
2025-04-30 06:43:22 -0700

    Still adding resolver

--- glox/parser/resolver.go
+++ glox/parser/resolver.go
@@ -26,32 +26,6 @@ func (s *scopes) pop() {
 	*s = (*s)[:len(*s)-1]
 }
 
-func (r *Resolver) visitBlockStmt(bs *BlockStmt) any {
-	r.beginScope()
-	r.resolveStmts(bs.statments)
-	r.endScope()
-	return nil
-}
-
-func (r *Resolver) visitVariableStmt(vs *VarStmt) any {
-	r.declare(vs.name)
-	if vs.initializer != nil {
-		r.resolveExpr(vs.initializer)
-	}
-	r.define(vs.name)
-	return nil
-}
-
-func (r *Resolver) VisitVariable(expr *Variable) any {
-	v, _ := (*r.scopes.peek())[expr.name.Lexeme]
-	if (len(*r.scopes) > 0) && v == false {
-		panic("cannot read local variable in its own initializer")
-	}
-
-	r.resolveLocal(expr, expr.name)
-	return nil
-}
-
 func (r *Resolver) resolveLocal(expr Expr, name *token.Token) {
 	for i, sc := range *r.scopes {
 		_, ok := (*sc)[name.Lexeme]
@@ -111,4 +85,109 @@ func (r *Resolver) define(name *token.Token) {
 	}
 
 	(*r.peek())[name.Lexeme] = true
+}
+
+func (r *Resolver) visitBlockStmt(bs *BlockStmt) any {
+	r.beginScope()
+	r.resolveStmts(bs.statments)
+	r.endScope()
+	return nil
+}
+
+func (r *Resolver) visitVariableStmt(vs *VarStmt) any {
+	r.declare(vs.name)
+	if vs.initializer != nil {
+		r.resolveExpr(vs.initializer)
+	}
+	r.define(vs.name)
+	return nil
+}
+
+func (r *Resolver) VisitVariable(expr *Variable) any {
+	v, _ := (*r.scopes.peek())[expr.name.Lexeme]
+	if (len(*r.scopes) > 0) && v == false {
+		panic("cannot read local variable in its own initializer")
+	}
+
+	r.resolveLocal(expr, expr.name)
+	return nil
+}
+
+func (r *Resolver) VisitAssign(expr *Assign) any {
+	r.resolveExpr(expr.value)
+	r.resolveLocal(expr, expr.name)
+	return nil
+}
+
+func (r *Resolver) visitFunctionStmt(fs *FunctionStmt) any {
+	r.declare(fs.name)
+	r.define(fs.name)
+	r.resolveFunction(fs)
+	return nil
+}
+
+func (r *Resolver) resolveFunction(fs *FunctionStmt) {
+	r.beginScope()
+	for _, param := range fs.params {
+		r.declare(param)
+		r.define(param)
+	}
+	r.resolveStmts(fs.body)
+	r.endScope()
+}
+
+func (r *Resolver) visitExpressionStmt(stmt *ExprStmt) any {
+	r.resolveExpr(stmt.Expr)
+	return nil
+}
+
+func (r *Resolver) visitIfStmt(stmt *IfStmt) any {
+	r.resolveExpr(stmt.condition)
+	r.resolveStmt(stmt.thenBranch)
+	if stmt.elseBranch != nil {
+		r.resolveStmt(stmt.elseBranch)
+	}
+	return nil
+}
+
+func (r *Resolver) visitPrintStmt(stmt *PrintStmt) any {
+	r.resolveExpr(stmt.Expr)
+	return nil
+}
+
+func (r *Resolver) visitReturnStmt(stmt *ReturnStmt) any {
+	if stmt.value != nil {
+		r.resolveExpr(stmt.value)
+	}
+	return nil
+}
+
+func (r *Resolver) visitWhileStmt(stmt *WhileStmt) any {
+	r.resolveExpr(stmt.condition)
+	r.resolveStmt(stmt.body)
+	return nil
+}
+
+func (r *Resolver) VisitBinary(expr *Binary) any {
+	r.resolveExpr(expr.Left)
+	r.resolveExpr(expr.Right)
+	return nil
+}
+
+func (r *Resolver) VisitCall(expr *CallExpr) any {
+	r.resolveExpr(expr.callee)
+
+	for _, argument := range expr.arguments {
+		r.resolveExpr(argument)
+	}
+	return nil
+}
+
+func (r *Resolver) VisitGrouping(expr *Grouping) any {
+	r.resolveExpr(expr.Expression)
+	return nil
+}
+
+func (r *Resolver) VisitLiteral(expr *Literal) any {
+	return nil
 }
